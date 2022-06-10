742fdfa9fcecc98ddddca8f135b04b323c690e95
Sessarego <107201250+Sessarego@users.noreply.github.com>
2022-06-10 09:36:09 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,3 +1,4 @@
 <?php
-include_once ("index.html");
+<composer class="json">include_once ("index.html");</composer>
+ $heroku <buildpacks:set>https://github.com/Sessarego/Biblioteca</buildpacks:set>
 ?>
